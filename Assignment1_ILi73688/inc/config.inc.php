df689428a757071bf4bbeda44efe920da458e0d8
Ian <gamrise@gmail.com>
2024-09-19 14:35:02 -0700

    Finished with config.inc.php set up

---
+++ Assignment1_ILi73688/inc/config.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+const numOfOrder = 4;
+const developer = "Ian";
+const appID = "300373688";
+const min_amount = 0;
+const max_amount = 10;
+const tax_percentage = 0.12;
+
+$items = 
+[
+    ["Item" => "Item A", "Price" => 30],
+    ["Item" => "Item B", "Price" => 30],
+    ["Item" => "Item C", "Price" => 40],
+];
+#value testing
+// foreach ($items as $key)
+// {
+//     echo $key["Item"], " ";
+//     echo $key["Price"], " ";
+//     echo "\n";
+// }
+// ;
+
+?>
